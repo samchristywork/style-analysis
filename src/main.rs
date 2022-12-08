d29a294fb7bff08f0a4294e29ebce9c330186fdb
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-08 17:31:34 -0600

    Feature: Pretty-print source code
    This simple example walks the source code using a TreeCursor and
    pretty-prints each node.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
-use tree_sitter::{Language, Parser};
+use tree_sitter::{Language, Node, Parser, TreeCursor};
+
+fn print_node(node: Node, level: usize) {
+    let source_code = "fn test() { println!(\"Hello, World!\"); }";
+
+    print!("{}", " ".repeat(level));
+    println!("{:?}", &source_code[node.start_byte()..node.end_byte()]);
+}
+
+fn traverse_tree(mut cursor: TreeCursor, level: usize) {
+    if !cursor.goto_first_child() {
+        return;
+    }
+    print_node(cursor.node(), level);
+
+    loop {
+        print_node(cursor.node(), level);
+
+        traverse_tree(cursor.clone(), level + 1);
+
+        if !cursor.goto_next_sibling() {
+            break;
+        }
+    }
+}
 
 fn main() {
     let mut parser = Parser::new();
@@ -13,5 +37,6 @@ fn main() {
     let source_code = "fn test() { println!(\"Hello, World!\"); }";
     let tree = parser.parse(source_code, None).unwrap();
     let root_node = tree.root_node();
-    println!("{:?}", root_node);
+
+    traverse_tree(root_node.walk(), 0);
 }
