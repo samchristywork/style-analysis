5b7e8a75f2b1788c880ea28a63accf0229d08bca
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-11 22:08:36 -0600

    Feature: Added example usage for tree traversal
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,26 @@
-use tree_sitter::{Node, TreeCursor};
+use tree_sitter::{Language, Parser};
 
 pub mod highlight;
+pub mod traverse;
 
 fn main() {
     let source_code = "fn test() {\n    print!(\"Hello, \");\n    println!(\"World!\");\n}";
     highlight::print(source_code);
 
-    print!("{}", " ".repeat(level));
-    // println!("{:?}", &source_code[node.start_byte()..node.end_byte()]);
-    println!("{:?}", node.kind());
-}
+    let mut parser = Parser::new();
 
-fn traverse_tree(mut cursor: TreeCursor, level: usize) {
-    if !cursor.goto_first_child() {
-        return;
+    extern "C" {
+        fn tree_sitter_rust() -> Language;
     }
-    print_node(cursor.node(), level);
 
-    loop {
-        print_node(cursor.node(), level);
+    let language = unsafe { tree_sitter_rust() };
+    parser.set_language(language).unwrap();
 
-        traverse_tree(cursor.clone(), level + 1);
+    let tree = parser.parse(source_code, None).unwrap();
+    let root_node = tree.root_node();
 
-        if !cursor.goto_next_sibling() {
-            break;
-        }
-    }
-}
-
-fn main() {
-    let source_code = "fn test() {\n    print!(\"Hello, \");\n    println!(\"World!\");\n}";
-    highlight::print(source_code);
+    traverse::traverse_tree(root_node.walk(), 0, |n, l| {
+        print!("{}", " ".repeat(l));
+        println!("{}", n.kind())
+    });
 }
