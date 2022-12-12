caa6a80b211845c9218dc6ee239226a31ab3d240
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-12 16:16:16 -0600

    Documentation: Added comments for each case tester
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/case.rs
+++ src/case.rs
@@ -1,4 +1,6 @@
+// camelCase
 pub fn is_camel(s: &str) -> bool {
+    // First character should be lowercase
     match s.chars().nth(0) {
         Some(c) => {
             if !c.is_ascii_lowercase() {
@@ -8,6 +10,7 @@ pub fn is_camel(s: &str) -> bool {
         None => return false,
     };
 
+    // All other characters should be uppercase or lowercase
     for c in s.chars() {
         if !(c.is_ascii_lowercase() || c.is_ascii_uppercase()) {
             return false;
@@ -16,7 +19,9 @@ pub fn is_camel(s: &str) -> bool {
     true
 }
 
+// PascalCase
 pub fn is_pascal(s: &str) -> bool {
+    // First character should be uppercase
     match s.chars().nth(0) {
         Some(c) => {
             if !c.is_ascii_uppercase() {
@@ -26,6 +31,7 @@ pub fn is_pascal(s: &str) -> bool {
         None => return false,
     };
 
+    // All other characters should be uppercase or lowercase
     for c in s.chars() {
         if !(c.is_ascii_lowercase() || c.is_ascii_uppercase()) {
             return false;
@@ -34,7 +40,9 @@ pub fn is_pascal(s: &str) -> bool {
     true
 }
 
+// SCREAMING_SNAKE
 pub fn is_screaming_snake(s: &str) -> bool {
+    // First character should be uppercase
     match s.chars().nth(0) {
         Some(c) => {
             if !c.is_ascii_uppercase() {
@@ -44,6 +52,7 @@ pub fn is_screaming_snake(s: &str) -> bool {
         None => return false,
     };
 
+    // Last character should be uppercase
     match s.chars().nth_back(0) {
         Some(c) => {
             if !c.is_ascii_uppercase() {
@@ -53,6 +62,7 @@ pub fn is_screaming_snake(s: &str) -> bool {
         None => return false,
     };
 
+    // All other characters should be uppercase or "_"
     for c in s.chars() {
         if !(c == '_' || c.is_ascii_uppercase()) {
             return false;
@@ -61,7 +71,9 @@ pub fn is_screaming_snake(s: &str) -> bool {
     true
 }
 
+// UPPERCASE
 pub fn is_uppercase(s: &str) -> bool {
+    // All characters should be uppercase
     for c in s.chars() {
         if !c.is_ascii_uppercase() {
             return false;
@@ -70,7 +82,9 @@ pub fn is_uppercase(s: &str) -> bool {
     true
 }
 
+// kebab-case
 pub fn is_kebab_case(s: &str) -> bool {
+    // First character should be lowercase
     match s.chars().nth_back(0) {
         Some(c) => {
             if !c.is_ascii_lowercase() {
@@ -80,6 +94,7 @@ pub fn is_kebab_case(s: &str) -> bool {
         None => return false,
     };
 
+    // Last character should be lowercase
     match s.chars().nth_back(0) {
         Some(c) => {
             if !c.is_ascii_lowercase() {
@@ -89,6 +104,7 @@ pub fn is_kebab_case(s: &str) -> bool {
         None => return false,
     };
 
+    // All other characters should be lowercase or "-"
     for c in s.chars() {
         if !(c == '-' || c.is_ascii_lowercase()) {
             return false;
@@ -97,7 +113,9 @@ pub fn is_kebab_case(s: &str) -> bool {
     true
 }
 
+// lowercase
 pub fn is_lowercase(s: &str) -> bool {
+    // All characters should be lowercase
     for c in s.chars() {
         if !c.is_ascii_lowercase() {
             return false;
@@ -106,7 +124,9 @@ pub fn is_lowercase(s: &str) -> bool {
     true
 }
 
+// snake_case
 pub fn is_snake_case(s: &str) -> bool {
+    // First character should be lowercase
     match s.chars().nth_back(0) {
         Some(c) => {
             if !c.is_ascii_lowercase() {
@@ -116,6 +136,7 @@ pub fn is_snake_case(s: &str) -> bool {
         None => return false,
     };
 
+    // Last character should be lowercase
     match s.chars().nth_back(0) {
         Some(c) => {
             if !c.is_ascii_lowercase() {
@@ -125,6 +146,7 @@ pub fn is_snake_case(s: &str) -> bool {
         None => return false,
     };
 
+    // All other characters should be lowercase or "_"
     for c in s.chars() {
         if !(c == '_' || c.is_ascii_lowercase()) {
             return false;
